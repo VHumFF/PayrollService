b5e49db751cc1e410fd425161d8332cfece051b8
VHumFF <108907994+VHumFF@users.noreply.github.com>
2024-10-15 12:21:08 +0800

    -add getPayrollList to payrollService interface

--- src/main/java/com/dcom/rmi/PayrollService.java
+++ src/main/java/com/dcom/rmi/PayrollService.java
@@ -1,13 +1,16 @@
 package com.dcom.rmi;
 
+import com.dcom.dataModel.Payroll;
 import com.dcom.dataModel.PayrollTemplate;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.List;
 import java.util.Map;
 
 public interface PayrollService extends Remote {
     Map<Integer, String> getPayrollListForUser(String token) throws RemoteException;
     PayrollTemplate generatePayroll(String token, int payrollId) throws RemoteException;
     boolean processPayroll(String token, int payrollId) throws RemoteException;
+    List<Payroll> getPayrollList(String token) throws RemoteException;
 }
